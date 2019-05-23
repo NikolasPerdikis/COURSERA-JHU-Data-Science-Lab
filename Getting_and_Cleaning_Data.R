4bede4a12cd24ba26e64ad2404abbed5cb46f20a
NotNikolas63500 <40383930+NotNikolas63500@users.noreply.github.com>
2019-05-23 21:30:38 +0200

    Update Getting_and_Cleaning_Data.R

--- Getting_and_Cleaning_Data.R
+++ Getting_and_Cleaning_Data.R
@@ -8,4 +8,12 @@ cran %>%
   filter(size_mb <= 0.5) %>%
   arrange(desc(size_mb))
   
-  # Your call to arrange() goes here
+# Your call to arrange() goes here
+
+#WEEK 3 Quiz
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(fileURL, destfile="week3q1.csv", method="curl")
+w3q1 <- read.csv("week3q1.csv",header=FALSE,skip=1,sep=",")
+
+# ACS == 3 AGS == 6 , columns 11,12
