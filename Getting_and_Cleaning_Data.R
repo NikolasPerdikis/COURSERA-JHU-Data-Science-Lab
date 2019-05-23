21d1264affe903c234df0090bec6cb74a688b831
NotNikolas63500 <40383930+NotNikolas63500@users.noreply.github.com>
2019-05-23 22:39:58 +0200

    Update Getting_and_Cleaning_Data.R

--- Getting_and_Cleaning_Data.R
+++ Getting_and_Cleaning_Data.R
@@ -17,3 +17,28 @@ download.file(fileURL, destfile="week3q1.csv", method="curl")
 w3q1 <- read.csv("week3q1.csv",header=FALSE,skip=1,sep=",")
 
 # ACS == 3 AGS == 6 , columns 11,12
+agricultureLogical<-(w3q1$V11==3 & w3q1$V12==6)
+#or you can rename the columns by their index
+names(w3q1)[11:12] <- c("ACS","AGS") and then do the same as abovelibrary(jpeg)
+
+fileURLJPEG="https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
+download.file(fileURLJPEG,destfile="jeff.jpg",method="curl")
+res <- readJPEG("jeff.jpg",native=TRUE)
+class(res)
+quantile(res,probs=c(0.3,0.8))
+
+fileURLq3_1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+download.file(fileURLq3_1,destfile="GDP.csv", method="curl")
+fileURLq3_2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+download.file(fileURLq3_2,destfile="EDU.csv", method="curl")
+dfEDU <- read.csv("EDU.csv")
+names(dfEDU)
+dfGDP <- read.csv("GDP.CSV", skip=5,header=FALSE, stringsAsFactors = FALSE)
+names(dfGDP)[1:2] <- c("CountryCode", "Ranking")
+head(dfGDP)
+# keep only Rankings 1:190
+dfGDP <- dfGDP[dfGDP$Ranking>=1,]
+dfGDP190 <- dfGDP[-c(191:193),]
+mergeddf <- merge(dfEDU,dfGDP190,by="CountryCode")
+
+  
