2f19d8e0be9a30610001b37a59fd1f19dc1a8bf8
NotNikolas63500 <nikolas.perdikis@gmail.com>
2019-05-24 22:33:26 +0200

    Update Getting_and_Cleaning_Data.R

--- Getting_and_Cleaning_Data.R
+++ Getting_and_Cleaning_Data.R
@@ -45,6 +45,30 @@ table(unlist(res))
  21251  21287 
      2      1  
  
+ library(httr)
+
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
+oauth_endpoints("github")
+
+# 2. To make your own application, register at
+#    https://github.com/settings/developers. Use any URL for the homepage URL
+#    (http://github.com is fine) and  http://localhost:1410 as the callback url
+#
+#    Replace your key and secret below.
+myapp <- oauth_app("NotNikolas63500",
+  key = "",
+  secret = ""
+)
+
+# 3. Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# 4. Use API
+gtoken <- config(token = github_token)
+req <- with_config(gtoken, GET("https://api.github.com/users/jtleek/repos"))
+stop_for_status(req)
+content(req)
  
 #END WEEK2
 
