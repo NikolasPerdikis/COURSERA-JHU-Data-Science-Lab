33762802a1ea84d8f38e137944fdc8f2364fa681
NotNikolas63500 <nikolas.perdikis@gmail.com>
2019-06-10 09:16:46 +0200

    Update R_Programming.R

--- R_Programming.R
+++ R_Programming.R
@@ -1,3 +1,6 @@
-# Keep here interesting code from the lessons
+# Keep here reuseable code snippets 
 east <- subset(pollution, region == "east")
 table(mpg$model) #shows how many models are in the dataset
+
+#convert a string to a date
+as.date(as.character(<integer field YYYYMMDD>),"%Y%m%d")
